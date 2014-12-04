75364489dda5ac883bb53664bc38ca596f1358ac
roguedjack <roguedjack@yahoo.fr>
2014-12-04 11:24:54 +0100

    debug fps display

--- src/rj/helmet/Main.hx
+++ src/rj/helmet/Main.hx
@@ -1,8 +1,10 @@
 package rj.helmet;
+import h2d.Text;
 import hxd.App;
 import hxd.Key;
 import hxd.Res;
-import rj.helmet.screens.IntroScreen;
+import hxd.res.FontBuilder;
+import rj.helmet.screens.TitleScreen;
 import rj.helmet.screens.PlayScreen;
 
 
@@ -19,15 +21,19 @@ class Main extends App {
 	public static inline var TILE_SIZE = 32;
 	public static inline var TILE_SHEET_COLUMNS = 8;
 	public static inline var TILE_SHEET_ROWS = 8;
-	public static inline var TILED_TILE_LAYER_NAME = "tiles";
+	public static inline var TILED_FLOOR_LAYER_NAME = "floor";
+	public static inline var TILED_WALLS_LAYER_NAME = "walls";
 	
 	public static var Instance:Main;
 		
 	public var world(default,null):World;
 	public var view(default, default):View;
 	public var screen(default, set):Screen;
-	public var introScreen(default,null):IntroScreen;
-	public var playScreen(default,null):PlayScreen;
+	public var introScreen(default,null):TitleScreen;
+	public var playScreen(default, null):PlayScreen;
+	
+	static inline var DEBUG_FPS = true;
+	var fpsText:Text;
 	
 	function set_screen(s:Screen) {
 		if (s == screen) {
@@ -50,7 +56,7 @@ class Main extends App {
 		world = new World(this);
 		view = null;
 		
-		introScreen = new IntroScreen(this);
+		introScreen = new TitleScreen(this);
 		playScreen = new PlayScreen(this);
 		screen = introScreen;
 		
@@ -60,6 +66,13 @@ class Main extends App {
 	
 	override function update(dt:Float) {
 		screen.update(1.0 / engine.fps);
+		if (DEBUG_FPS) {
+			if (fpsText == null) {
+				fpsText = new Text(FontBuilder.getFont("arial", 10), s2d);				
+				fpsText.setPos(s2d.width - 50, 0);
+			}
+			fpsText.text = 'fps:${Std.int(engine.fps)}';
+		}
 	}
 	
 	static function main() {
